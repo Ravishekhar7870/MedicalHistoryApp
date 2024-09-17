85b64669f528f2d1e5ca9538cc56af6c01eab172
Ravishekhar7870 <122727655+Ravishekhar7870@users.noreply.github.com>
2024-09-17 21:14:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config();
+import express from 'express'
+import { app } from './app.js';
+import DbConnect from './src/Db/DbConnect.js';
+const PORT=process.env.PORT || 8080
+DbConnect()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`app is listening on ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err);
+    console.log("connection failed!!!!!")
+})
---
+++ src/Routes/User.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { DoctorLogin, LogoutDoctor, LogoutPatient, PatientLogin, RefreshAcessAndRefreshTokenForDoctor, RefreshAcessAndRefreshTokenForPatient, RegisterDoctor, RegisterPatient } from "../Controllers/User.Controller.js";
+import { verifyJwtForDoctor, verifyJwtForPatient } from "../Middlewares/Auth.middleware.js";
+const router=Router();
+router.route('/registerPatient').post(RegisterPatient)
+router.route('/registerDoctor').post(RegisterDoctor)
+router.route('/loginPatient').post(PatientLogin)
+router.route('/loginDoctor').post(DoctorLogin)
+router.route('/logoutPatient').get(verifyJwtForPatient,LogoutPatient)
+router.route('/logoutDoctor').get(verifyJwtForDoctor,LogoutDoctor)
+router.route('/RefreshTokenPatient').get(RefreshAcessAndRefreshTokenForPatient)
+router.route('/RefreshTokenDoctor').get(RefreshAcessAndRefreshTokenForDoctor)
+export default router
