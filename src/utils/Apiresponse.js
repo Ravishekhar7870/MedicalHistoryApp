a5d5e102e8b5fa6c97d8f1512e4ed1a51d6b860f
Ravishekhar7870 <122727655+Ravishekhar7870@users.noreply.github.com>
2024-10-05 22:52:51 +0530

    Delete src directory

--- src/utils/Apiresponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse{
-    constructor(message="success",statuscode,data,success){
-        this.message=message;
-        this.statuscode=statuscode;
-        this.data=data;
-        this.success=statuscode<400;
-    }
-}
-export {ApiResponse}
